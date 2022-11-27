b701e6ce38ebc833ea9af0fe04e1655e4f49b0aa
Mikulas Peter <54554239+MikulasP@users.noreply.github.com>
2022-11-27 15:34:22 +0100

    Update aes.h

--- C/AES/aes.h
+++ C/AES/aes.h
@@ -34,228 +34,231 @@ SOFTWARE.
 #include <stdint.h>
 #include <stdlib.h>
 
-	/**
-	*	Egy byte kicserélése az sBox sorozatban lévő megfelelőjével.
-	*
-	*	@param <uint8_t>byte Kicserélésre szánt byte
-	*	
-	*	@returns uint8_t A megadott szám sBox megfelelője.
-	*/
-	uint8_t SubByteSingle(uint8_t byte);
+#define AES_MAX_BUFFER_SIZE    8 * ( 1000 /* 1 kB */ )    //Max buffer size on heap in megabytes -!!- MUST BE MULTIPLE OF 16 bytes -!!-
+/*
+* 
+*	Note: This size only restricts single buffers, NOT the whole program buffer size.
+* 
+*/
 
-	/// <summary>
-	/// Substitute bytes in block
-	/// </summary>
-	/// <param name="block">The blockof data to work on</param>
-	/**
-	*	Blokk byte-jainak kicserélése az sBox sorozatban lévő megfelelőjükkel
-	*
-	*	@param <uint8_t>byte Kicserélésre szánt byte
-	*/
-	void SubBytes(uint8_t* block);
+/**
+*	AES dataset to pass and receive data while processing.
+*/
+typedef struct AES_DATASET {
+	char* inputFileName;			///< Input file's name (file to process)
+	char* outputFileName;			///< Output file's name (processed file)
+	uint8_t* srcStream;				///< Pointer to source stream (stream to process)
+	uint8_t* dstStream;				///< Pointer to destination stream (processed stream)
+	size_t streamLen;				///< Source stream length (only used with stream operations)
+	size_t progress;				///< Progress feedback (nuber of bytes processed) Can be read out to get progress with multithreaded operations
+} AES_DATASET;
 
-	/// <summary>
-	/// Inverse substitute bytes in block
-	/// </summary>
-	/// <param name="block">The blockof data to work on</param>
-	/**
-	*	Blokk byte-jainak kicserélése az inverz sBox sorozatban lévő megfelelőjükkel
-	*
-	*	@param <uint8_t>byte Kicserélésre szánt byte
-	*/
-	void SubBytesInv(uint8_t* block);
+/**
+* 	Encrypt a single 16 byte long block*
+*
+* 	@param	<uint8_t*>block		Array containing the data to be encrypted
+*
+*/
+void EncryptBlock(uint8_t* block);
 
-	/// <summary>
-	/// Shift rows left in block 
-	/// </summary>
-	/// <param name="block">The blockof data to work on</param>
-	/**
-	* 	Feldolgozás alatt lévő blokk sorainak balra eltolása
-	* 
-	* 	@param <uint8_t*>block		Feldolgozás alatt lévő blokk
-	* 
-	*/
-	void ShiftRowsLeft(uint8_t* block);
+/**
+* 	Encrypt stream of bytes
+* 	@param	<uint8_t*>src			Source stream
+*	@param <uint8_t*>dst			Destination stream
+* 	@param <size_t>length			Source length
+*
+*/
+void EncryptStream(uint8_t* src, uint8_t* dst, size_t length);
 
-	/// <summary>
-	/// Shift rows right in block 
-	/// </summary>
-	/// <param name="block">The blockof data to work on</param>
-	/**
-	* 	Feldolgozás alatt lévő blokk sorainak jobbra eltolása
-	* 
-	* 	@param <uint8_t*>block		Feldolgozás alatt lévő blokk
-	* 
-	*/
-	void ShiftRowsRight(uint8_t* block);
+/**
+* 	Encrypt stream at original position
+*
+*	@param <uint8_t*>stream			Source stream
+* 	@param <size_t>length			Source length
+*
+*/
+void EncryptStreamOrigin(uint8_t* stream, size_t length);
 
-	/// <summary>
-	/// Mix columns round
-	/// </summary>
-	/// <param name="block">The blockof data to work on</param>
-	/**
-	* 	Feldolgozás alatt lévő blokk oszlopainak keverése
-	* 
-	* 	@param <uint8_t*>block		Feldolgozás alatt lévő blokk
-	* 
-	*/
-	void MixColumns(uint8_t* block);
+/**
+*	Encrypt and pad a stream of bytes
+*
+*	@param <uint8_t*>src			Source stream
+*	@param <size_t>length			Source length
+*	@param <size_t*>dstLength		Finished stream length
+*	@param <bool>attachPadding		Attach padding to last block
+*
+*	@returns	<uint8_t*>		Pointer to encrypted data
+*/
+uint8_t* Encrypt(uint8_t* src, size_t length, size_t* streamLength, bool attachPadding);
 
-	/// <summary>
-	/// Inverse mix columns round
-	/// </summary>
-	/// <param name="block">The blockof data to work on</param>
-	/**
-	* 	Feldolgozás alatt lévő blokk oszlopainak inverz keverése
-	* 
-	* 	@param <uint8_t*>block		Feldolgozás alatt lévő blokk
-	* 
-	*/
-	void MixColumnsInv(uint8_t* block);
+/**
+*	Encrypt and save file
+*
+*	@param <AES_DATASET*>dataset	Pointer to AES_DATASET struct
+*
+*	@returns <int>					Exit code (See documentation page X) (later)
+*/
+//int EncryptFileToFile(char* inputFileName, char* outputFileName, size_t* encryptedSizePtr, size_t* fullSizePtr);
+int EncryptFileToFile(AES_DATASET* dataset);
 
-	/// <summary>
-	/// Galois field GF(2^8) multiplication
-	/// </summary>
-	/// <param name="multiplicant"> Multiplicant</param>
-	/// <param name="multiplier">Multiplier (1, 2, 3, 9, 11, 13, 14)</param>
-	/// <returns>The product</returns>
-	uint8_t GFMult(uint8_t multiplier, uint16_t multiplicant);
+/**
+*	Encrypr file tostream
+*
+*	@param	<char*>inputFileName	Source file name
+*	@param	<uint8_t*>stream		Encrypted file stream
+*	@param	<size_t*>				Length of the encrypted stream
+*
+*	@returns <int>					Exit code (See documentation page X) (later)
+*/
+//int EncryptFileToStream(char* inputFileName, uint8_t* stream, size_t* streamLength);
 
-	/*
-	uint8_t GaloisFieldMult(uint8_t multiplier, uint8_t multiplicant);
-	*/
+/**
+* 	Decrypt a single 16 byte long block*
+*
+* 	@param	<uint8_t*>block			Array containing the data to be decrypted
+*
+*/
+void DecryptBlock(uint8_t* block);
 
-	/// <summary>
-	/// Add key to a blockof data
-	/// </summary>
-	/// <param name="block">The block to add key</param>
-	/// <param name="keyNum">Number of the key stage</param>
-	/**
-	* 	Kulcs hozzáadása a feldolgozás alatt lévő blokkhoz.
-	* 	
-	* 	@param <uint8_t*>block		Feldolgozás alatt lévő blokk
-	* 	@param <uint8_t>keyNum		A blokkhoz hozzáadandó kulcs sorszáma
-	* 
-	*/
-	void AddRoundKey(uint8_t* block, uint8_t keyNum);
+/**
+* 	Decrypt stream of bytes
+*
+* 	@param	<uint8_t*>src			Source stream
+*	@param <uint8_t*>dst			Destination stream
+* 	@param <size_t>length			Source length
+*
+*/
+void DecryptStream(uint8_t* src, uint8_t* dst, size_t length);
 
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="key"></param>
-	/// <param name="keyNum"></param>
-	/**
-	* 	AES kulcs kibővítése
-	* 
-	* 	@param <uint8_t*>srcKey		Forrás kulcs
-	* 	@param <uint8_t*>dstKey		Kibővített kulcs
-	* 	@param <uint8_t>keyNum		Kibővített kulcs sorszáma
-	* 
-	*/
-	void ExpandKey(uint8_t* srcKey, uint8_t* dstKey, uint8_t keyNum);
+/**
+* 	Decrypt stream at original position
+*
+*	@param <uint8_t*>stream			Source stream
+* 	@param <size_t>length			Source length
+*
+*/
+void DecryptStreamOrigin(uint8_t* stream, size_t length);
 
-	/// <summary>
-	/// Calculate all key stages
-	/// </summary>
-	/// <param name="key"></param>
-	/**
-	* 	AES kulcs fázisok kiszámítása és tárolása
-	* 
-	* 	@param	<char*>key			A titkosításhoz használt kulcs
-	* 
-	*/
-	void CalculateKeys(char* key);
+/**
+*	Decrypt and pad a stream of bytes
+*
+*	@param <uint8_t*>src			Source stream
+*	@param <size_t>length			Source length
+*	@param <size_t*>streamength		Finished stream length
+*	@param <bool>removePadding		Remove padding from the last block
+*
+*	@returns	<uint8_t*>			Pointer to decrypted data
+*/
+uint8_t* Decrypt(uint8_t* src, size_t length, size_t* streamLength, bool removePadding);
 
-	/// <summary>
-	/// Encrypt a single 16 byte long block
-	/// </summary>
-	/// <param name="block">Array containing the data to be encrypted</param>
-	/**
-	* 	Egy blokk titkosítása és felülírása.
-	* 
-	* 	@param	<uint8_t*>block		Titkosítandó blokk
-	* 
-	*/
-	void EncryptBlock(uint8_t* block);
+/**
+*/
+//int DecryptFileToFile(char* inputFileName, char* outputFileName, size_t* decryptedSizePtr, size_t* fullSizePtr);
+int DecryptFileToFile(AES_DATASET* dataset);
 
-	/// <summary>
-	/// Encrypt a stream of bytes from src to dst.
-	/// </summary>
-	/// <param name="block">Array containing the data to be encrypted</param>
-	/**
-	* 	Adatok titkosítása, és visszaírása a memóriába.
-	* 
-	* 	@param	<uint8_t*>src		A titkosítandó adatok helye a memóriában
-	*	@param 	<uint8_t*>dst		A titkosított adatok helye a memóriában
-	* 	@param 	<size_t>length		A titkosítandó adatok byte száma
-	* 
-	*/
-	void EncryptStream(uint8_t* src, uint8_t* dst, size_t length);
+/**
+*	Substitute a single byte
+*
+*	@param <uint8_t>byte The byte to replace*
+* 
+*	@returns uint8_t Corresponding byte according to sBox
+*/
+uint8_t SubByteSingle(uint8_t byte);
 
-	/// <summary>
-	/// Encrypt a stream of bytes in given array. | Note: For now only works with streams divisible by 16
-	/// </summary>
-	/// <param name="stream"></param>
-	/**
-	* 	Adatok titkosítása, és felülírása a memóriában.
-	* 
-	*	@param 	<uint8_t*>stream	A titkosítandó adatok helye a memóriában
-	* 	@param 	<size_t>length		A titkosítandó adatok byte száma
-	* 
-	*/
-	void EncryptStreamOrigin(uint8_t* stream, size_t length);
+/**
+*	Substitute bytes in block
+*
+*	@param <uint8_t>byte The block of data to work on
+*/
+void SubBytes(uint8_t* block);
 
-	/**
-	*	...
-	* 
-	*	@param	<uint8_t*>src		A titkosítandó adatok helye a memóriában
-	*	@param	<size_t>length		A titkosítandó adatok byte száma
-	*	@param	<size_t*>dstLength	A titkosított adatok byte száma
-	* 
-	*	@returns	<uint8_t*>		Pointer a titkosított adatok helyére.
-	*/
-	uint8_t* Encrypt(uint8_t* src, size_t length, size_t* streamLength);
+/**
+*	Inverse substitute bytes in block
+*
+*	@param <uint8_t>byte The blockof data to work on
+*/
+void SubBytesInv(uint8_t* block);
 
-	/// <summary>
-	/// 
-	/// </summary>
-	/**
-	* 	Egy blokk visszafejtése és felülírása.
-	* 
-	* 	@param	<uint8_t*>block		Visszafejtendő blokk.ó
-	* 
-	*/
-	void DecryptBlock(uint8_t* block);
+/**
+* 	Shift rows left in block
+*
+* 	@param <uint8_t*>block		The block of data to work on
+*
+*/
+void ShiftRowsLeft(uint8_t* block);
 
-	/// <summary>
-	/// Decrypt a previously encrypted stream of bytes from src to dst.
-	/// </summary>
-	/// <param name="stream"></param>
-	/**
-	* 	Adatok visszafejtése, és visszaírása a memóriába.
-	* 
-	* 	@param	<uint8_t*>src		A visszafejtendő adatok helye a memóriában
-	*	@param 	<uint8_t*>dst		A visszafejtett adatok helye a memóriában
-	* 	@param 	<size_t>length		A visszafejtendő adatok byte száma
-	* 
-	*/
-	void DecryptStream(uint8_t* src, uint8_t* dst, size_t length);
+/**
+* 	Shift rows right in block
+*
+* 	@param <uint8_t*>block		The block of data to work on
+*
+*/
+void ShiftRowsRight(uint8_t* block);
 
+/**
+* 	Mix columns round
+*
+* 	@param <uint8_t*>block		The block of data to work on
+*
+*/
+void MixColumns(uint8_t* block);
 
-	/// <summary>
-	/// Decrypt a previously encrypted stream of bytes in given array. | Note: Stream length must be divisible by 16!
-	/// </summary>
-	/// <param name="stream"></param>
-	/**
-	* 	Adatok visszafejtése, és felülírása a memóriában.
-	* 
-	*	@param 	<uint8_t*>stream	A visszafejtendő adatok helye a memóriában
-	* 	@param 	<size_t>length		A visszafejtendő adatok byte száma
-	* 
-	*/
-	void DecryptStreamOrigin(uint8_t* stream, size_t length);
+/**
+* 	Inverse mix columns round
+*
+* 	@param <uint8_t*>block		The blockof data to work on
+*
+*/
+void MixColumnsInv(uint8_t* block);
 
-	/**
-	* 
-	*/
-	uint8_t* Decrypt(uint8_t* src, size_t length, size_t* streamLength);
+/**
+	Galois field GF(2^8) multiplication
+
+	@param <uint8_t>Multiplicant
+	@param <uint8_t>Multiplier
+*/
+uint8_t GFMult(uint8_t multiplier, uint16_t multiplicant);
+
+/**
+* 	Add key to a blockof data
+*
+* 	@param <uint8_t*>block		The block to add key
+* 	@param <uint8_t>keyNum		Number of the key stage
+*
+*/
+void AddRoundKey(uint8_t* block, uint8_t keyNum);
+
+/**
+* 	Expand AES keys
+*
+* 	@param <uint8_t*>srcKey		Source key
+* 	@param <uint8_t*>dstKey		Destination key
+* 	@param <uint8_t>keyNum		Expanded key’s number
+*
+*/
+void ExpandKey(uint8_t* srcKey, uint8_t* dstKey, uint8_t keyNum);
+
+/**
+* 	Calculate aes key stages (Input must be 16 bytes long!)
+*
+* 	@param	<char*>key			AES Secret key
+*
+*/
+void CalculateKeys(char* key);
+
+/**
+*	Get a file's size int bytes
+* 
+*	@param <FILE*>file			Input file
+* 
+*	@returns <size_t>			The input file's size in byzes
+*/
+size_t GetFileSizeBytes(FILE* file);
+
+/**
+*	Set key from a string given by a user (most likely)
+* 
+*	@param <char*>str				String containing the key
+*/
+void SetKeyString(char* str);
+
